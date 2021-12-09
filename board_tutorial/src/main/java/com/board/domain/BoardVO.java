f31492dd5cba1a49bad9737e94a091146e02878e
thankdesert <thankdesert@gmail.com>
2021-12-10 00:24:56 +0900

    2021-12-10

---
+++ board_tutorial/src/main/java/com/board/domain/BoardVO.java
@@ -0,0 +1,45 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	private int bno,viewCnt;
+	private String title,content,writer;
+	private Date regDate;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+}
